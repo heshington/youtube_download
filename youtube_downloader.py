ea10f83b1687ee06ee4cfaa39e976620a9c7efeb
Hiren <Hiren@pikpok.com>
2021-03-10 16:32:53 +1300

    second commit, adding script, req.txt and virutual env files

---
+++ youtube_downloader.py
@@ -0,0 +1,29 @@
+from pytube import YouTube
+# misc
+import os
+import shutil
+import math
+import datetime
+# plots
+
+import matplotlib.pyplot as plt
+
+# image operation
+import cv2
+
+url = input("Enter the youtube URL you want to download ")
+#video = YouTube('https://www.youtube.com/watch?v=vT3GUKuAzIs')
+video = YouTube(url)
+video.streams.all()
+
+
+print(video.streams.filter(file_extension = "mp4").all())
+
+
+
+selected_stream =  input("Enter the stream number you want to download ")
+
+
+
+
+video.streams.get_by_itag(selected_stream).download()
