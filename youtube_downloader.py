44760fe5edb18b598a01aec55995e9748beec596
Hiren <Hiren@pikpok.com>
2021-03-10 16:37:38 +1300

    changed user promot

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -16,14 +16,8 @@ url = input("Enter the youtube URL you want to download ")
 video = YouTube(url)
 video.streams.all()
 
-
 print(video.streams.filter(file_extension = "mp4").all())
 
-
-
-selected_stream =  input("Enter the stream number you want to download ")
-
-
-
+selected_stream =  input("Enter the itag number you want to download ")
 
 video.streams.get_by_itag(selected_stream).download()
